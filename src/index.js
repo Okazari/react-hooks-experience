5cdf9f4803aacd4927db6cce23c54a9dc3a32325
Benoit Laurent <benoit.jacques.felix@gmail.com>
2018-11-05 18:19:09 +0100

    🐛 better with rootEl define

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-
-ReactDOM.render(<App />, document.getElementById('root'))
+const rootEl = document.getElementById('root')
+ReactDOM.render(<App />, rootEl)
 
 if (module.hot) {
   module.hot.accept('./App', () => {
@@ -11,6 +11,7 @@ if (module.hot) {
     ReactDOM.render(<NextApp />, rootEl)
   })
 }
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
